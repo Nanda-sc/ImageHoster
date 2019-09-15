ac78c666169bc461ee2536b1cde499928d3ba639
Nanda <nanda.applications@gmail.com>
2019-09-15 17:06:48 +0530

    Password validation - Solution

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Controller
@@ -40,9 +42,29 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, Model model) {
+
+        String password = user.getPassword();
+
+        Pattern letter = Pattern.compile("[a-zA-z]");
+        Pattern digit = Pattern.compile("[0-9]");
+        Pattern spChar = Pattern.compile("[^A-Za-z0-9]");
+        Matcher m1 = letter.matcher(password);
+        Matcher m2 = digit.matcher(password);
+        Matcher m3 = spChar.matcher(password);
+
+        if(m1.find() && m2.find() && m3.find()){
+            userService.registerUser(user);
+            return "redirect:/users/login";
+        }else{
+            String error = "Password must contain at least 1 alphabet, 1 number & 1 special character";
+            User user1 = new User();
+            UserProfile profile = new UserProfile();
+            user1.setProfile(profile);
+            model.addAttribute("User", user1);
+            model.addAttribute("passwordTypeError",error);
+            return "users/registration";
+        }
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
